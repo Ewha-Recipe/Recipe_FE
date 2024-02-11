c000a2be34b9fa7aba418e0856fdf12814669e84
nomellc <nomellc012@gmail.com>
2024-02-12 04:09:13 +0900

    Revert "refactor: Simplify the signup screen"
    This reverts commit 7daf3497a9a089901f675ecac64c4b2c2c59c7f1.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignupOne from './SignupOne';
+import SignupTwo from './SignupTwo';
+import SignupThree from './SignupThree';
 
 function App() {
   return (
-    <div className="App">
-      Hello Ewha Recipe
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/signup" exact component={SignupOne} />
+          <Route path="/signup-two" component={SignupTwo} />
+          <Route path="/signup-complete" component={SignupThree} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SignupForm from './signup/SignupForm';
-import LoginForm from './login/LoginForm';
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signup" component={SignupForm} />
-        <Route path="/login" component={LoginForm} />
-      </Switch>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
