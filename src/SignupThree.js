b5bfff5e728c5efc799b44d00fdee1aee39706e8
nomellc <nomellc012@gmail.com>
2024-02-12 04:09:23 +0900

    Revert "chore: Move the location of the image file"
    This reverts commit 4a6b79cf6e24c1c0a69a5335ea5389d9cf3e2c26.

---
+++ src/SignupThree.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import logo from './logo.png';
+
+const SignupComplete = () => {
+  const history = useHistory();
+
+  const handleMyPage = () => {
+    history.push('/mypage');
+  }
+
+  const handleExit = () => {
+    history.push('/');
+  }
+
+  return (
+    <div className="container">
+      <img className="logo" src={logo} alt="logo" />
+      <div className="progress-indicator">
+        <div className="indicator">아이디/비밀번호 설정</div>
+        <div className="line" />
+        <div className="indicator">프로필 설정</div>
+        <div className="line" />
+        <div className="indicator active">회원가입 완료</div>
+      </div>
+      <div className="completion-message">
+        <p>회원가입이 성공적으로 완료되었습니다!</p>
+      </div>
+      <div className="buttons">
+        <button
+          type="button"
+          className="move-button"
+          id="mypage-button"
+          onClick={handleMyPage}
+        >
+          마이페이지로 이동
+        </button>
+        <button
+          type="button"
+          className="move-button"
+          id="cancel-button"
+          onClick={handleExit}
+        >
+          홈으로 이동
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignupComplete;
