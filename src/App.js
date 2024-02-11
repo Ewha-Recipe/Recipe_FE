7daf3497a9a089901f675ecac64c4b2c2c59c7f1
nomellc <nomellc012@gmail.com>
2024-02-12 03:57:05 +0900

    refactor: Simplify the signup screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignupOne from './SignupOne';
-import SignupTwo from './SignupTwo';
-import SignupThree from './SignupThree';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/signup" exact component={SignupOne} />
-          <Route path="/signup-two" component={SignupTwo} />
-          <Route path="/signup-complete" component={SignupThree} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      Hello Ewha Recipe
+    </div>
   );
 }
 
