fde7f3c3b522c9ec01c8d170c21812b818bc3828
Yujin Jeong <127232686+nomellc@users.noreply.github.com>
2024-02-20 19:42:53 +0900

    chore: Rename signUp folder to signup folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SignupForm } from './signUp/SignupForm';
+import { SignupForm } from './signup/SignupForm';
 import { Home } from './home';
 import { CreatePost } from './post/CreatePost';
 
